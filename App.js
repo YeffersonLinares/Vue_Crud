2b430815e66f6870b2f91af8a7efd14c1d20f15a
fcgalindom <fcgalindo@gmail.com>
2021-02-24 12:09:58 -0500

    buscar

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-url = 'http://crud_vue.test/'
+url = 'http://laravel_crud_vue.test/'
 
 const main = new Vue({
     el: '#main',
     created: function(){
         this.getUsers()
+        this.imprimir()
     },
     mounted: function(){
         //setInterval(this.getUsers,5000)
@@ -15,10 +16,23 @@ const main = new Vue({
         getUsers: function(){
             axios.get(url).then(response => {
                 this.lists  = response.data
+              
+            })
+        },
+        imprimir: function(){
+
+            search =  document.getElementById('search').value;
+            url = 'http://laravel_crud_vue.test/user/'+search
+             
+            
+            axios.get(url).then(response => {
+                this.lists  = response.data
                 this.lists.forEach(element => {
                     console.log(element.name)
                 });
-            })
-        },
+            });
+        }
+
+       
     }
 })
